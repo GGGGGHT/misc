450f5acfed9abed34fb502e94ebdd42442116d6c
ggggght <wz9712203617@gmail.com>
2022-09-15 18:52:45 +0800

    feat: first submit

---
+++ README.md
@@ -0,0 +1,1 @@
+learn java use jdk17 
---
+++ src/test/java/com/ggggght/misc/MiscApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ggggght.misc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
